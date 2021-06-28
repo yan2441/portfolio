3064acde42bb5c8b86527de009fbc52ddf33139b
Yassine najem <yasen2441@gmail.com>
2021-06-28 23:24:13 +0100

    add skills component to app.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import NavBar from './components/navbar';
 import AboutMe from './components/aboutme';
 import Projects from './components/projects';
 import Contact from './components/contact';
+import Skills from './components/skills';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <AboutMe />
+      <Skills />
       <Projects />
-      <Contact/> 
+      <Contact />
     </div>
   );
 }
