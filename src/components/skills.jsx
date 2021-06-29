17d1be50283dcdd4e1215d1d69095c937adf7992
Yassine najem <yasen2441@gmail.com>
2021-06-29 01:39:13 +0100

    map logos object into skills component

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import logos from './svgloader';
+
 
 
 const Skills = () => {
+
   return (
 
     <section className="mx-auto">
       <div className="container px-5 mx-auto lg:px-24 lg:py-20">
         <div className="grid grid-cols-3 gap-16 text-center lg:grid-cols-6">
-          <div className="flex items-center justify-center ">
-            <img src="https://d33wubrfki0l68.cloudfront.net/e018a9cb3c198579040cd7c76efc4319d9d73065/78dee/logos/notion.svg" alt="Todoist Logo" className="block object-contain h-10 filter grayscale"></img>
-          </div>
+          {
+            Object.entries(logos).map(([key, value]) => {
+              return <div className="flex items-center justify-center ">
+                <img src={value} alt={key} className="block object-contain h-10 "></img>
+              </div>
+            })
+          }
         </div>
       </div>
     </section>
