c078c8818f19df436524a9848a15a8bec316537a
Yassine najem <yasen2441@gmail.com>
2021-06-29 01:36:18 +0100

    create the skills component

---
+++ src/components/skills.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const Skills = () => {
+  return (
+
+    <section className="mx-auto">
+      <div className="container px-5 mx-auto lg:px-24 lg:py-20">
+        <div className="grid grid-cols-3 gap-16 text-center lg:grid-cols-6">
+          <div className="flex items-center justify-center ">
+            <img src="https://d33wubrfki0l68.cloudfront.net/e018a9cb3c198579040cd7c76efc4319d9d73065/78dee/logos/notion.svg" alt="Todoist Logo" className="block object-contain h-10 filter grayscale"></img>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  );
+}
+
+export default Skills;
