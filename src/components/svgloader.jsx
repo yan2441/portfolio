1c16f777617db4a98306598d141549aac12c2896
Yassine najem <yasen2441@gmail.com>
2021-06-29 01:37:53 +0100

    create file return all logos as one object

---
+++ src/components/svgloader.jsx
@@ -0,0 +1,24 @@
+import css from "../assets/skills/css-3.svg"
+import html from "../assets/skills/html-5.svg"
+import node from "../assets/skills/nodejs.svg"
+import react from "../assets/skills/react.svg"
+import express from "../assets/skills/express.svg"
+import tailcss from "../assets/skills/tailwindcss-icon.svg"
+import mongo from "../assets/skills/mongodb.svg"
+import mysql from "../assets/skills/mysql-6.svg"
+import java from "../assets/skills/java.svg"
+
+
+export default {
+
+  html,
+  css,
+  node,
+  express,
+  react,
+  tailcss,
+  mongo,
+  mysql,
+  java
+
+}
