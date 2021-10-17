ce393d7f5c4730339b6bc321f59d7edb15651c77
Yassine najem <yasen2441@gmail.com>
2021-10-17 19:27:12 +0100

    fix skills section

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -9,11 +9,14 @@ const Skills = () => {
 
     <section className="mx-auto">
       <div className="container px-5 mx-auto lg:px-24 lg:py-20">
-        <div className="grid grid-cols-3 gap-16 text-center lg:grid-cols-6">
+        <div className="flex items-center justify-center ">
+          <h3 className="m-10 mb-16 text-2xl font-bold text-blue-600">Technologies and skills</h3>
+        </div>
+        <div className="grid grid-cols-2 gap-16 text-center lg:grid-cols-8">
           {
             Object.entries(logos).map(([key, value]) => {
               return <div className="flex items-center justify-center ">
-                <img src={value} alt={key} className="block object-contain h-10 "></img>
+                <img src={value} key={key} alt={key} className="block object-contain h-16 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 "></img>
               </div>
             })
           }
