c99d7831a36082e7f63c411d3ad616e1e5c275e6
Yassine najem <yasen2441@gmail.com>
2022-01-11 22:29:08 +0100

    add projects images with slider

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import SimpleImageSlider from 'react-simple-image-slider';
 export default function ProjectCard({ item }) {
+  const { images } = item;
   return (
-    <div className="w-4/5 p-6 mx-auto md:w-1/2">
+    <div className="w-[80%] p-6 mx-auto md:w-1/2">
       <div className="shadow-xl rounded-xl bg-blueGray-50">
-        <img className="object-cover object-center w-full lg:h-48 md:h-36 rounded-t-xl" src="https://dummyimage.com/720x400/aaaaaa/8693ac" alt="project" />
+        <div className="object-cover object-center w-[30%] md:h-auto rounded-t-xl">
+          <SimpleImageSlider
+            style={{ margin: 0, padding: 0 }}
+            width={500}
+            height={320}
+            images={images}
+            showNavs={true}
+            navSize={40}
+            slideDuration={3}
+            loop={true}
+            navStyle={1}
+            autoPlay={true}
+            autoPlayDelay={Math.floor(Math.random() * (15 - 2) + 2)}
+          />
+
+        </div>
         <div className="p-4 lg:p-6 bg-blueGray-50">
           <h2 className="mb-3 text-2xl font-semibold tracking-wide text-black uppercase title-font">{item.name}</h2>
           <h1 className="mx-auto mb-3 font-medium leading-none tracking-tight text-black text-m lg:text-l title-font">{item.describe}</h1>
         </div>
-        <div className="px-6 py-2 bg-blue-200 rounded-b-xl">
+        <div className="px-6 py-2 bg-gray-200 rounded-b-xl">
           <a href="/" className="inline-flex items-center mt-auto font-semibold text-blue-600 lg:mb-0 hover:text-black " title="github repo"> take a look » </a>
         </div>
       </div>
     </div>
   )
 }
+
+
+
+
--- src/components/projectsList.js
+++ src/components/projectsList.js
@@ -1,24 +1,76 @@
+import docs from '../assets/mockup/docs.png';
+import docs1 from '../assets/mockup/docs1.png';
+import chat from '../assets/mockup/chat.png';
+import chat1 from '../assets/mockup/chat1.png';
+import home from '../assets/mockup/home.png';
+import home1 from '../assets/mockup/home1.png';
+import DW1 from '../assets/mockup/DW1.png';
+import DW2 from '../assets/mockup/DW2.png';
+import DW3 from '../assets/mockup/DW3.png';
+import FT1 from '../assets/mockup/FT1.png';
+import FT2 from '../assets/mockup/FT2.png';
+import FT3 from '../assets/mockup/FT3.png';
+import GH1 from '../assets/mockup/GH1.png';
+import GH2 from '../assets/mockup/GH2.png';
+
 const projects = [
   {
     id: 1,
     name: 'Text editor (Google doc clone)',
-    describe: "create and edit you document and save on cloud with firebase"
+    describe: "create and edit you document and save on cloud with firebase",
+    images: [
+      { url: docs },
+      { url: docs1 },
+    ],
   },
   {
     id: 2,
     name: 'React chat app',
-    describe: "chat with you friend using this privet chatting site"
+    describe: "chat with you friend using this privet chatting site",
+    images: [
+      { url: chat },
+      { url: chat1 },
+    ],
+  },
+  {
+    id: 6,
+    name: 'homefinder web app',
+    describe: "find and discover your next home",
+    images: [
+      { url: home },
+      { url: home1 },
+    ],
   },
   {
     id: 3,
     name: 'DoneWithIt',
-    describe: "market place to sell used stuff you dont want it any more"
+    describe: "market place to sell used stuff you dont want it any more",
+    images: [
+      { url: DW1 },
+      { url: DW2 },
+      { url: DW3 },
+    ],
   },
   {
     id: 4,
     name: 'Github seeker',
-    describe: "find github user and there repository"
+    describe: "find github user and there repository",
+    images: [
+      { url: GH1 },
+      { url: GH2 },
+    ],
   },
+  {
+    id: 5,
+    name: 'Gym app',
+    describe: "track your workout and save it to cloud",
+    images: [
+      { url: FT1 },
+      { url: FT2 },
+      { url: FT3 },
+    ],
+  },
+
 ]
 
 export default projects
