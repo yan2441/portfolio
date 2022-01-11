3891316377dc96bdcb19a418f252880c8e6e1578
Yassine najem <yasen2441@gmail.com>
2022-01-12 00:13:44 +0100

    install email dep

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -12,8 +12,7 @@ const Contact = () => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		//const res = await emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
-		//console.log(res)
+		const res = await emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
 		e.target.reset();
 	}
 
