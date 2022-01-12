a9ca522af4925c3d9110ec96d01e08a674de0992
Yassine najem <yasen2441@gmail.com>
2022-01-12 22:45:26 +0100

    change image Carousel

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
-import SimpleImageSlider from 'react-simple-image-slider';
+import { Carousel } from 'react-responsive-carousel';
+// eslint-disable-next-line
+import styles from 'react-responsive-carousel/lib/styles/carousel.min.css';
 export default function ProjectCard({ item }) {
   const { images } = item;
-  return (
-    <div className="w-[80%] p-6 mx-auto md:w-1/2">
-      <div className="shadow-xl rounded-xl bg-blueGray-50">
-        <div className="object-cover object-center w-[30%] md:h-auto rounded-t-xl">
-          <SimpleImageSlider
-            style={{ margin: 0, padding: 0 }}
-            width={500}
-            height={320}
-            images={images}
-            showNavs={true}
-            navSize={40}
-            slideDuration={3}
-            loop={true}
-            navStyle={1}
-            autoPlay={true}
-            autoPlayDelay={Math.floor(Math.random() * (15 - 2) + 2)}
-          />
 
+  return (
+    <div className="w-[80%] p-6 mx-auto lg:w-1/2">
+      <div className="shadow-xl rounded-sm bg-blueGray-50">
+        <div className="object-cover object-center w-[90%] md:h-auto rounded-t-xl">
+          <Carousel autoPlay={true} transitionTime={'1000'} showThumbs={false} infiniteLoop={true} stopOnHover={true} showIndicators={false} showStatus={false}>
+            {images.map((image, index) => (
+              <div key={index}>
+                <img src={image} alt={image} />
+              </div>
+            ))}
+          </Carousel>
         </div>
         <div className="p-4 lg:p-6 bg-blueGray-50">
           <h2 className="mb-3 text-2xl font-semibold tracking-wide text-black uppercase title-font">{item.name}</h2>
           <h1 className="mx-auto mb-3 font-medium leading-none tracking-tight text-black text-m lg:text-l title-font">{item.describe}</h1>
         </div>
-        <div className="px-6 py-2 bg-gray-200 rounded-b-xl">
-          <a href="/" className="inline-flex items-center mt-auto font-semibold text-blue-600 lg:mb-0 hover:text-black " title="github repo"> take a look » </a>
+        <div className="px-6 py-2 bg-gray-200 rounded-b-sm">
+          <a href={item.link} target={'_blank'} rel='noreferrer' className="inline-flex items-center mt-auto font-semibold text-blue-600 lg:mb-0 hover:text-black " title="github repo"> take a look » </a>
         </div>
       </div>
     </div>
--- src/components/projectsList.js
+++ src/components/projectsList.js
@@ -18,57 +18,43 @@ const projects = [
     id: 1,
     name: 'Text editor (Google doc clone)',
     describe: "create and edit you document and save on cloud with firebase",
-    images: [
-      { url: docs },
-      { url: docs1 },
-    ],
-  },
-  {
-    id: 2,
-    name: 'React chat app',
-    describe: "chat with you friend using this privet chatting site",
-    images: [
-      { url: chat },
-      { url: chat1 },
-    ],
+    images: [docs, docs1],
+    link: 'http://te-nextjs.vercel.app/'
   },
   {
     id: 6,
     name: 'homefinder web app',
     describe: "find and discover your next home",
-    images: [
-      { url: home },
-      { url: home1 },
-    ],
+    images: [home, home1],
+    link: 'https://homefinder.vercel.app/'
+  },
+  {
+    id: 2,
+    name: 'React chat app',
+    describe: "chat with you friend using this privet chatting site",
+    images: [chat, chat1],
+    link: 'https://rechat-react-app.netlify.app/'
   },
   {
     id: 3,
     name: 'DoneWithIt',
     describe: "market place to sell used stuff you dont want it any more",
-    images: [
-      { url: DW1 },
-      { url: DW2 },
-      { url: DW3 },
-    ],
+    images: [DW1, DW2, DW3],
+    link: 'https://expo.dev/@yas2441/DoneWithIt'
   },
   {
     id: 4,
     name: 'Github seeker',
     describe: "find github user and there repository",
-    images: [
-      { url: GH1 },
-      { url: GH2 },
-    ],
+    images: [GH1, GH2],
+    link: 'https://expo.dev/@yas2441/github-app'
   },
   {
     id: 5,
     name: 'Gym app',
     describe: "track your workout and save it to cloud",
-    images: [
-      { url: FT1 },
-      { url: FT2 },
-      { url: FT3 },
-    ],
+    images: [FT1, FT2, FT3],
+    link: 'https://expo.dev/@yas2441/testTask'
   },
 
 ]
