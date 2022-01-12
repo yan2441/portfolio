8e705002deb87608da73af7369d0d8881eebad48
Yassine najem <yasen2441@gmail.com>
2022-01-12 22:46:04 +0100

    add socialmedia links

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import SocialMedia from './SocialMedia'
 import emailjs from 'emailjs-com';
 
 
@@ -17,11 +18,12 @@ const Contact = () => {
 	}
 
 	return (
-		<div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-coolGray-800 dark:text-coolGray-100">
+		<div className="grid max-w-screen-xl grid-cols-1 gap-16 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-coolGray-800 dark:text-coolGray-100">
 			<div className="flex flex-col justify-between">
 				<div className="space-y-2">
 					<h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's work!</h2>
-					<div className="dark:text-coolGray-400">Send me an E-mail</div>
+					<div className="text-lg">Send me an E-mail</div>
+					<SocialMedia size={40} />
 				</div>
 				<img src={logo} alt="Contact" className="p-6 h-52 md:h-64"></img>
 			</div>
@@ -38,7 +40,7 @@ const Contact = () => {
 					<label htmlFor="message" className="text-sm">Message</label>
 					<textarea id="message" name="message" placeholder="Your Message" rows="3" className="w-full p-3 bg-gray-200 border-gray-400 rounded dark:bg-coolGray-800"></textarea>
 				</div>
-				<input type="submit" value="Send Massage" className="w-full p-3 text-sm font-bold tracking-wide uppercase bg-blue-600 border-black rounded"></input>
+				<input type="submit" value="Send Massage" className="w-full p-3 text-sm font-bold tracking-wide uppercase text-white bg-blue-600 border-black rounded"></input>
 			</form>
 		</div>
 	);
